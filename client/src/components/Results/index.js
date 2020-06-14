685147dc1222a8243ae721953f9c6bec11194195
Reeka <reekamaharaj@gmail.com>
2020-06-14 16:26:02 -0500

    styling for result card

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,28 +1,52 @@
 import React from "react";
+import "./style.css";
 
 const Result = (props) => {
     return (
-            <ul className="list-group">
-            {props.books.map(book => {
+        <div>
+            {props.books.map((book) => {
                 return (
-                    <div>
-                        <li className="list-group-item" key={book._id}>
-                            <img src={book.image} alt={book.title} />
-                            <strong> 
-                                {book.title} by {book.authors} 
-                            </strong>
-                            <button className="btn btn-info" id={book.id} onClick={book.saveBook}>Save</button>
-                            <a href={book.link} target="_blank">
-                                    <button className="btn btn-info">More Info</button>
-                                </a>
-                        </li>
-                        <li className="list-group-item">
-                            {book.description}
-                        </li>
+                    <div className="row justify-content-md-center m-4 p-4 border">
+                        <div className="col-md-12" key={book._id}>
+                            <div id="picandbtn">
+                                <img src={book.image} alt={book.title} />
+                                <p className="my-2">
+                                    <strong>{book.title}</strong>
+                                </p>
+                                <p>
+                                    <strong>by {book.authors}</strong>
+                                </p>
+                                <div
+                                    className="btn-group mr-2"
+                                    role="group"
+                                    aria-label="Basic example">
+                                    <button
+                                        type="button"
+                                        className="btn btn-info"
+                                        id={book.id}
+                                        onClick={book.saveBook}>
+                                        Save
+                                    </button>
+                                </div>
+                                <div
+                                    className="btn-group"
+                                    role="group"
+                                    aria-label="Basic example">
+                                    <a href={book.link} target="_blank">
+                                        <button
+                                            type="button"
+                                            className="btn btn-info">
+                                            More Info
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="">{book.description}</div>
+                        </div>
                     </div>
                 );
             })}
-            </ul>
+        </div>
     );
 };
 export default Result;
