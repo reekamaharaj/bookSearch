a4386226b43be18b43ba2ba9aae83643861a1530
Reeka <reekamaharaj@gmail.com>
2020-06-14 22:39:56 -0500

    trying to deploy

---
+++ client/build/precache-manifest.3f91834a7f759053c8a705cb8774793f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9245bc20376729d2afa88a7ca1e8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73c4f33c7fd8aabd166",
+    "url": "/static/css/main.ecb22420.chunk.css"
+  },
+  {
+    "revision": "0b486c419b7bd968783f",
+    "url": "/static/js/2.f921c8aa.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.f921c8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73c4f33c7fd8aabd166",
+    "url": "/static/js/main.b0594283.chunk.js"
+  },
+  {
+    "revision": "51ac0df437c1c295d630",
+    "url": "/static/js/runtime-main.0aaab9aa.js"
+  }
+]);
